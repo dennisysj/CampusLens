e591a06f715ba1cda62588979a6eeb8ff7e48c0c
demurean <arielle.felicia@gmail.com>
2025-10-05 04:15:49 -0700

    testing stuff

--- frontend/sockettest.js
+++ frontend/sockettest.js
@@ -1,14 +1,20 @@
 const io = require('socket.io-client');
 const socket = io('http://localhost:3000');
 
+// to note:
+// socket.on is for receiving events
+// socket.emit is for sending events
+
 socket.on('connect', () => {
   console.log('Connected:', socket.id);
   socket.emit('user_position', { "lat": 49.28, "lon": -122.91 });
 });
+// ^ that format is good
 
 socket.on('welcome', (data) => {
   console.log('Welcome:', JSON.stringify(data));
 });
+// ^ doing stringify is ugly
 
 socket.on('position_status', (data) => {
   console.log('Position update:', data);
@@ -17,3 +23,7 @@ socket.on('position_status', (data) => {
 socket.on('off_boundaries', (data) => {
   console.log('Off boundaries:', data);
 });
+
+socket.emit('ping', {info: 'test from client'});
+
+//socket.disconnect();
